b385385770d94a30a9f14cb45a4dc2d05843d61f
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-03 15:28:06 +0000

    add second map() test .js

--- workshop/index.js
+++ workshop/index.js
@@ -1,3 +1,9 @@
-function map(array) {
-  return array;
+// function map(array, fn) {
+//   return array;
+// }
+
+function map(array, fn) {
+  const el = array[0];
+  const newEl = fn(el);
+  return [newEl];
 }
