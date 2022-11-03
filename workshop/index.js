6565ef8a6dc9cbaff18c52ec19530893efc5a899
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-03 15:51:08 +0000

    add filter()

--- workshop/index.js
+++ workshop/index.js
@@ -1,13 +1,18 @@
+// Testing map()
+
 // function map(array, fn) {
 //   return array;
 // }
 
-function map(array, fn) {
-  const result = [];
-  for (const item of array) {
-    const newEl = fn(item);
-    result.push(newEl)
-  }
+// function map(array, fn) {
+//   const result = [];
+//   for (const item of array) {
+//     const newEl = fn(item);
+//     result.push(newEl)
+//   }
   
-  return result;
-}
+//   return result;
+// }
+
+// Testing filter()
+
--- workshop/index.test.js
+++ workshop/index.test.js
@@ -1,20 +1,22 @@
 // testing map()
 
-test("map() should return an array with the same number of elements", () => {
-  const result = map([1], () => {});
-  equal(result.length, 1);
-});
+// test("map() should return an array with the same number of elements", () => {
+//   const result = map([1], () => {});
+//   equal(result.length, 1);
+// });
 
-test("map() should transform each element of the array using the fn argument", () => {
-  const result = map([1], (x) => x + 1);
-  equal(result[0], 2);
-});
+// test("map() should transform each element of the array using the fn argument", () => {
+//   const result = map([1], (x) => x + 1);
+//   equal(result[0], 2);
+// });
 
-test('map() should transform all elements in the array using the fn argument', () => {
-  const actual = map([1, 2, 3], (x) => x * 2);
-  const expected = [2, 4, 6];
-  equal(actual[0], expected[0])
-  equal(actual[1], expected[1])
-  equal(actual[2], expected[2])
-})
+// test('map() should transform all elements in the array using the fn argument', () => {
+//   const actual = map([1, 2, 3], (x) => x * 2);
+//   const expected = [2, 4, 6];
+//   equal(actual[0], expected[0])
+//   equal(actual[1], expected[1])
+//   equal(actual[2], expected[2])
+// })
+
+// testing filter
 
